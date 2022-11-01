309e670e0f402419999390f7d0e89f892cacfc9d
Breno Rodrigues <brenocartoon@hotmail.com>
2022-11-01 20:22:38 -0300

    feat: updating home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
-      <h1>Main Page</h1>
+      <h1>Contagem: {props.count}</h1>
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/pools/count');
+  const data = await response.json();
+
+  return {
+    props: {
+      count: data.count,
+    }
+  };
+};
