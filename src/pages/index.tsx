af8e27499c4968f579cd4c83fca25e59069f59a0
Breno Rodrigues <brenocartoon@hotmail.com>
2022-11-03 23:35:27 -0300

    feat: create home page with all styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,76 @@
+import Image from 'next/image';
+import appPreviewImg from '../assets/app_preview.png';
+import logo from '../assets/logo.svg';
+import usersAvatar from '../assets/avatares.png';
+import iconCheck from '../assets/icon.svg';
+
 interface HomeProps {
   count: number;
 }
 
 export default function Home(props: HomeProps) {
   return (
-    <div>
-      <h1>Contagem: {props.count}</h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
+      <main>
+        <Image
+          src={logo}
+          alt="Logotipo."
+          quality={100}
+        />
+
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatar} alt="" />
+          <strong className="text-gray-100 text-lg">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão utilizando
+          </strong>
+        </div>
+
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            type="text"
+            required
+            placeholder="Qual o nome do seu bolão?"
+          />
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-600"
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 text-gray-100 flex items-center justify-between">
+          <div className="flex items-center gap-6">
+            <Image src={iconCheck} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div className="w-px h-14 bg-gray-600" />
+          <div className="flex items-center gap-6">
+            <Image src={iconCheck} alt="" />
+            <div className="flex flex-col">
+              <span  className="font-bold text-2xl">+192.034</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Image
+        src={appPreviewImg}
+        alt="Celulares com prévia da aplicação."
+        quality={100}
+      />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const { url } = require('inspector');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ['./src/**/*.tsx',],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+      backgroundImage: {
+        app: 'url(/app_bg.png)',
+      },
+      colors: {
+        gray: {
+          100: '#E1E2E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214',
+        },
+        ignite: {
+          500: '#129E57',
+        },
+        yellow: {
+          500: '#F7DD43',
+          600: '#E5CD3D',
+        },
+      },
+    },
   },
   plugins: [],
 }
